dd4c9e96e5e7a3627fba5e52f61fd8005146c4a8
Agustin-T <65094491+Agustin-T@users.noreply.github.com>
2021-10-11 12:14:27 -0300

    add to big file

--- buenasPracticas.js
+++ buenasPracticas.js
@@ -290,3 +290,21 @@ console.log(listaAnimales);
 console.log(typeof listaAnimales);
 console.log(nuevaLista);
 console.log(typeof nuevaLista);
+
+//-----------------------------------------------------------
+// test of rendimiento de JS
+
+// cosole.time(name) retorna cuanto tarda ejecutar todo el codigo hasta cosole.timeEnd(name)
+console.time('myCode')
+var animales = ['perro', 'gato','pez'];
+var l = animales.length;
+
+for (var i = 0; i < 10000; i++){
+    animales[i] = i
+}
+
+//analiza el timepo hasta here (tener same name for work it)
+console.timeEnd('myCode')
+
+
+// el testeo de rendimiento da distinto resultado en cada browcer ya que tiene distinta fprma de trabajo
