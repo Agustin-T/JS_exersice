d5abb6a631b36dbaea5b02d5bbbf9082bac35057
Agustin-T <65094491+Agustin-T@users.noreply.github.com>
2021-10-11 12:49:43 -0300

    minimizacion y ofuscacion
    > add butom in index.html
    > new test

--- test.js
+++ test.js
@@ -4,19 +4,34 @@
 // Tool: jscompress.com
 // using fremework tienen minimizadores de codigo
 
-var tienda = {
-    nombre: "Tienda las 4 esquinas",
-    calcular: function(costo1, costo2) {
-        return costo1 + costo2;
-    },
-    saludar: function() {
-        var mensaje = "Hola ";
-        return mensaje;
-    }
-};
+// var tienda = {
+//     nombre: "Tienda las 4 esquinas",
+//     calcular: function(costo1, costo2) {
+//         return costo1 + costo2;
+//     },
+//     saludar: function() {
+//         var mensaje = "Hola ";
+//         return mensaje;
+//     }
+// };
 
-var btnSaludar = document.getElementById("btnSaludar");
+// var btnSaludar = document.getElementById("btnSaludar");
 
-btnSaludar.addEventListener('click', function() {
-    console.log(tienda.saludar());
-});
+// btnSaludar.addEventListener('click', function() {
+//     console.log(tienda.saludar());
+// });
+
+// with jscompress.com
+var tienda={nombre:"Tienda las 4 esquinas",calcular:function(n,a){return n+a},saludar:function(){return"Hola "}},btnSaludar=document.getElementById("btnSaludar");btnSaludar.addEventListener("click",function(){console.log(tienda.saludar())});
+//si es SENCIBLE EL CODIGO DESCARGAR UN MINIFICADOR, SI NO USA jscompress.com
+
+//  Osfucacion: volver ilegible para el programador
+//  Usando javascriptobfuscator.com/Javascript-Obfuscator.aspx
+
+
+var _0x57fd=["\x54\x69\x65\x6E\x64\x61\x20\x6C\x61\x73\x20\x34\x20\x65\x73\x71\x75\x69\x6E\x61\x73","\x48\x6F\x6C\x61\x20","\x62\x74\x6E\x53\x61\x6C\x75\x64\x61\x72","\x67\x65\x74\x45\x6C\x65\x6D\x65\x6E\x74\x42\x79\x49\x64","\x63\x6C\x69\x63\x6B","\x73\x61\x6C\x75\x64\x61\x72","\x6C\x6F\x67","\x61\x64\x64\x45\x76\x65\x6E\x74\x4C\x69\x73\x74\x65\x6E\x65\x72"];var tienda={nombre:_0x57fd[0],calcular:function(_0xc437x2,_0xc437x3){return _0xc437x2+ _0xc437x3},saludar:function(){var _0xc437x4=_0x57fd[1];return _0xc437x4}};var btnSaludar=document[_0x57fd[3]](_0x57fd[2]);btnSaludar[_0x57fd[7]](_0x57fd[4],function(){console[_0x57fd[6]](tienda[_0x57fd[5]]())})
+
+console.log("Nuestro codigo ha sigo ofuscado")
+console.log(tienda.nombre);
+console.log(tienda.calcular(20, 32));
+console.log(tienda.saludar());
