5266e00d6f6961fecc97c7c4f1c672b383f5eba5
Agustin-T <65094491+Agustin-T@users.noreply.github.com>
2021-10-11 11:52:35 -0300

    repir error in index
    > not was search the file

--- test.js
+++ test.js
@@ -1,3 +1,20 @@
+//USing JSON 
+// obtine el archivo productos.json ejecutando jquery
 $.getJSON('productos.json', function(productos){
-    console.log(productos)
+    console.log(productos);
+
+    var totalProductos = productos.length;
+
+    for(var i = 0; i < totalProductos; i++){
+        console.log(productos[i].nombre)
+    }
 })
+
+// coneccion con una conection url para acceder(no funciona el ejemplo url)
+
+// var url = "https://query.yahooapis.com/v1/public/yql?q=select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='são paulo, sp')&format=json&callback=callbackFunction";
+// $.getJSON(url, function(productos){
+//     console.log(productos);
+
+
+// })
