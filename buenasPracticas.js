cccb050aebd112d0ad7c0bb7193612420984580c
Agustin-T <65094491+Agustin-T@users.noreply.github.com>
2021-10-11 12:03:58 -0300

    add to big file

--- buenasPracticas.js
+++ buenasPracticas.js
@@ -273,4 +273,20 @@ $.getJSON('productos.json', function(productos){
 // })
 
 //-----------------------------------------------------------
+// join vs concat
 
+// concatenzar valiables 
+var animales = ['perro', 'gato','pez']
+
+// retorna como una cadena te texto
+// es mas simple
+var listaAnimales = animales.join(", ");
+// retorna un arreglo sumando la ','
+// es mas complejo, pero si quiero trabajar con cada uno de los elementos uso concat
+var nuevaLista = animales.concat(",");
+
+
+console.log(listaAnimales);
+console.log(typeof listaAnimales);
+console.log(nuevaLista);
+console.log(typeof nuevaLista);
