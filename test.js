3fca9dfa237699a47e74a3171c4d1b13096b7d53
Agustin-T <65094491+Agustin-T@users.noreply.github.com>
2021-10-11 12:01:34 -0300

    push 1:
    > delete query in index
    > comparation with Join() and concat()

--- test.js
+++ test.js
@@ -1,6 +1,13 @@
 // concatenzar valiables 
 var animales = ['perro', 'gato','pez']
 
+// retorna como una cadena te texto
 var listaAnimales = animales.join(", ");
+// retorna un arreglo sumando la ','
+var nuevaLista = animales.concat(",");
+
 
 console.log(listaAnimales);
+console.log(typeof listaAnimales);
+console.log(nuevaLista);
+console.log(typeof nuevaLista);
