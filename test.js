f2de105214b407f765eeb458c4e137c3ffb5109f
Agustin-T <65094491+Agustin-T@users.noreply.github.com>
2021-10-12 18:56:18 -0300

    patrones especiales
    Trabajar con caracteres especiales para repetir patrones
    > test.js

--- test.js
+++ test.js
@@ -1,2 +1,14 @@
 'use strick'
 
+var expresion1 = /\d/;
+
+console.log(expresion1.test('1997'));
+
+// + (1 o mas veces),* (1 o mas veces sin importar si existe)
+
+var expresion2 = /\d+/;
+
+console.log(expresion2.test('')); // false:porque por lo menos no hay una repeticion
+var expresion3 = /\d*/;
+
+console.log(expresion3.test('')); //True
