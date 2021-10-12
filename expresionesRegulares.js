e43d5db09fe7258d7bcf4ab3a937e9d9b582f5e4
Agustin-T <65094491+Agustin-T@users.noreply.github.com>
2021-10-12 11:55:18 -0300

    add to big file
    expresionesRegulares.js

--- expresionesRegulares.js
+++ expresionesRegulares.js
@@ -18,3 +18,14 @@ var expresion2 = /abc/;
 var parrafo = /text1\/n text2/;
 //-----------------------------------------------------------
 // Evaluacion de primeras expresiones
+// ejemplo 1
+
+var expresion1 = new RegExp('abc');
+// test() retorna true si encuentra lo que esta entre parentesis, (patron)
+console.log(expresion1.test('abcdef'));
+
+
+// ejemplo 2
+var expresion2 = /hola mundo/;
+console.log(expresion2.test('El primer programa es hola mundo'));
+//-----------------------------------------------------------
--- test.js
+++ test.js
@@ -1,12 +1,2 @@
 'use strick'
-
-// ejemplo 1
-
-var expresion1 = new RegExp('abc');
-// test() retorna true si encuentra lo que esta entre parentesis, (patron)
-console.log(expresion1.test('abcdef'));
-
-
-// ejemplo 2
-var expresion2 = /hola mundo/;
-console.log(expresion2.test('El primer programa es hola mundo'));
+var expresion1 = /hola mundo/;
