3a8b70b4b962284630f3f68a3bd1b581dde62c94
Agustin-T <65094491+Agustin-T@users.noreply.github.com>
2021-10-12 11:42:19 -0300

    add to big file
    > expresionesRegulares.js

--- expresionesRegulares.js
+++ expresionesRegulares.js
@@ -1,1 +1,18 @@
  'use strick'
+
+ 
+console.log('hola');
+//-----------------------------------------------------------
+// expresion regular
+
+// var regEx = 
+//objeto que permite realiza varias operacion, como robar un patron y como ejecutarlo
+//usar con expresiones grandes
+var expresion1 = new RegExp('abc');
+
+//ahce lo mismo
+//
+var expresion2 = /abc/;
+
+//usar con expresiones cortas
+var parrafo = /text1\/n text2/;
