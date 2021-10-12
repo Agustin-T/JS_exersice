76bdbae9e2fc5762085e0b578cf0fa127054164f
Agustin-T <65094491+Agustin-T@users.noreply.github.com>
2021-10-12 12:51:50 -0300

    add to big file
    > espresionesRegulares.js

--- expresionesRegulares.js
+++ expresionesRegulares.js
@@ -46,6 +46,22 @@ console.log(expresion3.test('name@mail.com'));// True
 //-----------------------------------------------------------
 //  Uso de ayajos para grupos de caracteres
 
+/*
+\d  Cualquier numero
+\w  Cualquier letra o número
+\s  CUalquier espacio en blanco incluido tabs, salto de linea o similares
+\D  Cualquier caracter que no sea un numero
+\W  cualquier caracter que no sea un espacio en blanco
+\S  Cualquier caracter que no sea un espacio en blanco
+.   Cualquier caracter excepto nuevas lineas
+*/
+
+var expresion1 = /\d\d-\d\d-\d\d\d\d/;
+
+console.log(expresion1.test('29-06-2007'));// true
+console.log(expresion1.test('29-jun-2007'));// false
+
+//29-06-2007
 
 //-----------------------------------------------------------
 // Coincidencias y negaciones de expresiones
