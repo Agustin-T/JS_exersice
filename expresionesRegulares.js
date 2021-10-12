1dc8335f28fa9830754dc163bf9574bb9a767bb3
Agustin-T <65094491+Agustin-T@users.noreply.github.com>
2021-10-12 19:36:43 -0300

    add to the big file
    > expresionesRegulares.js

--- expresionesRegulares.js
+++ expresionesRegulares.js
@@ -113,6 +113,11 @@ console.log(expresion3.test('Hola mudo'))
 //  Conteo de patrones de expresiones regulares
 
 
+// {minimo, maximo} de 1 a dos digitos
+var expresion1 = /\d{1,2}-\d{1,2}-\d{4} \d{1,2}:\d{2}/;
+
+console.log(expresion1.test('27-6-2007 11:45'))// true
+
 //-----------------------------------------------------------
 //  Saber y Agrupar los patrones segun tipos y funciones
 //-----------------------------------------------------------
--- test.js
+++ test.js
@@ -1,6 +1,2 @@
 'use strick'
 
-// {minimo, maximo} de 1 a dos digitos
-var expresion1 = /\d{1,2}-\d{1,2}-\d{4} \d{1,2}:\d{2}/;
-
-console.log(expresion1.test('27-6-2007 11:45'))// true
