24187aeb73de9c6cd5d9f6afa374002c77065eca
Agustin-T <65094491+Agustin-T@users.noreply.github.com>
2021-10-13 11:14:19 -0300

    add to big file
    > expresionesRegulares.js

--- test.js
+++ test.js
@@ -1,10 +1,3 @@
 'use strick'
 
 
-// both them work with the same objetive: return a object about what index makes what is demanded 
-// desde la variable
-var expresion1 = /\d+/.exec('Del año 1984');
-console.log(expresion1);
-
-// in console.log
-console.log('Hasta 1994'.match(/\d+/))
