b2eda0d9abc7f6652b9300d80ef6c7be42f3d814
Agustin-T <65094491+Agustin-T@users.noreply.github.com>
2021-10-13 11:13:36 -0300

    exec and match
    > test.js

--- test.js
+++ test.js
@@ -1,8 +1,10 @@
 'use strick'
 
-// + es uno a mas repeticiones
-//() subexpresiones
-var expresion1 = /Woo+(hoo+)+/i;
 
-// Woohooooooooo!
-console.log(expresion1.test('Woohooooohooooohooooohooooo'));
+// both them work with the same objetive: return a object about what index makes what is demanded 
+// desde la variable
+var expresion1 = /\d+/.exec('Del año 1984');
+console.log(expresion1);
+
+// in console.log
+console.log('Hasta 1994'.match(/\d+/))
