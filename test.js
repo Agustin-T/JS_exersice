62c442b618639cc70a9dc00e06782d09a5e69d33
Agustin-T <65094491+Agustin-T@users.noreply.github.com>
2021-10-13 11:03:52 -0300

    subexpression
    > test.js

--- test.js
+++ test.js
@@ -1,3 +1,8 @@
 'use strick'
 
+// + es uno a mas repeticiones
+//() subexpresiones
+var expresion1 = /Woo+(hoo+)+/i;
 
+// Woohooooooooo!
+console.log(expresion1.test('Woohooooohooooohooooohooooo'));
