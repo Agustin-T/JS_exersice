ed7dbb3e82e503e452d521744dada55eaa425086
Agustin-T <65094491+Agustin-T@users.noreply.github.com>
2021-10-15 09:46:30 -0300

    operador "pipa"
    > test.js

--- test.js
+++ test.js
@@ -1,2 +1,14 @@
 'use strick'
 
+// "|" significa alternativa (OR)
+
+var expresion1 = /pollo/;
+var expresion2 = /pollo|res|pescado/;
+var expresion3 = /pollo|res|pescado/;
+console.log(expresion1);
+console.log(expresion2);
+console.log(expresion3);
+
+console.log(expresion1.test('El cliente pidio pollo'));// True
+console.log(expresion2.test('El cliente pidio pescado'));// True
+console.log(expresion2.test('El cliente pidio tacos'));//False
