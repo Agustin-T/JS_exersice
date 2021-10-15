5568cea38b8655eab924c3bc3cc2c005ee7eebda
Agustin-T <65094491+Agustin-T@users.noreply.github.com>
2021-10-15 09:57:06 -0300

    add to big file
    > expresioensRegulares.js

--- expresionesRegulares.js
+++ expresionesRegulares.js
@@ -182,7 +182,18 @@ console.log(expresion2.test('El cliente pidio tacos'));//False
 //-----------------------------------------------------------
 //  Patrones al incio y al final de una expresion
 
+//Poder analizar las primeras palabras al inicio y final
+// ^ Inicio (alt + 94)
+// $ Fin
+var expresionInicio = /^Any/;
+var expresionFin = /JavaScript$/;
+var expresionInicioFin = /^[^Any]|JavaScript$/;
 
+
+console.log(expresionInicio.test('Any app that can be imagined can be made in JavaScript'));
+console.log(expresionFin.test('Any app that can be imagined can be made in JavaScript'));
+console.log(expresionInicioFin.test('Any app that can be imagined can be made in JavaScript'));
+console.log(expresionInicioFin.test('Any app that can be imagined can be made in Python'));
 
 
 //-----------------------------------------------------------
