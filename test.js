109cc8384945a9be3dba7cb3f3f0488fe1187049
Agustin-T <65094491+Agustin-T@users.noreply.github.com>
2021-10-15 09:41:14 -0300

    word limit
    > test.js

--- test.js
+++ test.js
@@ -1,3 +1,9 @@
 'use strick'
 
+//\b sirve para poner un limite
 
+var expresion1 = /\bcat\b/;
+var expresion2 = /\bcat\b/;
+
+console.log(expresion1.test('cat'));// True
+console.log(expresion2.test('categoria'));// False
