a2e83fc3da5173cee664dbc213e2e84a8ebb8b95
Agustin-T <65094491+Agustin-T@users.noreply.github.com>
2021-10-15 10:29:08 -0300

    add to big file
    > expresionesRegulares.js

--- expresionesRegulares.js
+++ expresionesRegulares.js
@@ -216,7 +216,20 @@ console.log(mensaje2.replace(/i/g, 'o'));
 //-----------------------------------------------------------
 //  Metodo de busqueda en las cadenas de texto
 
+var mensaje = 'Esta historia continuará...';
+var mensaje2 = '      Esta historia continuará...';
+var mensaje3 = 'Estahistoriacontinuará...';
+var mensaje4 = 'Estahistoriacontinuara';
 
+// \W  Cualquier caracter que no sea alfanumerico (espacio)
+// \S cualquier caracter que no sea un espaci en Blanco (o busca caracteres especiales)
+
+console.log(mensaje.search(/\W/));
+console.log(mensaje.search(/\W/g));
+// retorna la primera incidencia, sin importar si agregar /g
+
+console.log(mensaje3.search(/\S/));
+console.log(mensaje4.search(/\W/));
 
 //-----------------------------------------------------------
 //  La propiedad LasrIndex en expresiones regulares
