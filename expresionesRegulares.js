42a1e2f1a96196fdea257f00e06891f9a67b1085
Agustin-T <65094491+Agustin-T@users.noreply.github.com>
2021-10-15 09:47:08 -0300

    add to big file
    > expresionesRegulares.js

--- expresionesRegulares.js
+++ expresionesRegulares.js
@@ -165,7 +165,18 @@ console.log(expresion2.test('categoria'));// False
 
 //-----------------------------------------------------------   
 //  Uso de patrones opcionales en tue expresiones regulares 
+// "|" significa alternativa (OR)
 
+var expresion1 = /pollo/;
+var expresion2 = /pollo|res|pescado/;
+var expresion3 = /pollo|res|pescado/;
+console.log(expresion1);
+console.log(expresion2);
+console.log(expresion3);
+
+console.log(expresion1.test('El cliente pidio pollo'));// True
+console.log(expresion2.test('El cliente pidio pescado'));// True
+console.log(expresion2.test('El cliente pidio tacos'));//False
 
 
 //-----------------------------------------------------------
