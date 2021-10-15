991e84edc01e25678bd90c623022efd148fe7719
Agustin-T <65094491+Agustin-T@users.noreply.github.com>
2021-10-15 09:56:02 -0300

    detect patterns to analyze
    > test.js

--- test.js
+++ test.js
@@ -1,2 +1,14 @@
 'use strick'
 
+//Poder analizar las primeras palabras al inicio y final
+// ^ Inicio (alt + 94)
+// $ Fin
+var expresionInicio = /^Any/;
+var expresionFin = /JavaScript$/;
+var expresionInicioFin = /^[^Any]|JavaScript$/;
+
+
+console.log(expresionInicio.test('Any app that can be imagined can be made in JavaScript'));
+console.log(expresionFin.test('Any app that can be imagined can be made in JavaScript'));
+console.log(expresionInicioFin.test('Any app that can be imagined can be made in JavaScript'));
+console.log(expresionInicioFin.test('Any app that can be imagined can be made in Python'));
