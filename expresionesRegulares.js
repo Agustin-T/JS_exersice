a0408245adac8ee74901f7cb4a812145425f9e66
Agustin-T <65094491+Agustin-T@users.noreply.github.com>
2021-10-19 11:09:02 -0300

    add to big file
    > expresionesRegulares.js
    > test.js

--- expresionesRegulares.js
+++ expresionesRegulares.js
@@ -264,7 +264,18 @@ console.log(expresion3.exec('Pedi 4 tacos y 1 agua'));//15
 //-----------------------------------------------------------
 //  ciclos  y evaluaciones en la expresiones regulares
 
+var mensaje = 'Llegaban por montones, primero 10, despues 20, despues 35 y se duplicaban hasta llegar a 1000';
 
+//encontrar un patron un numero repetitivo de manera global
+var patron = /\b(\d+)\b/g;
+
+// donde guardaremos le evaluacion
+var match;
+
+while(match = patron.exec(mensaje)){
+    console.log(match);
+    console.log('Se encontro',match[1], ' en ', match.index);
+}
 
 //-----------------------------------------------------------
 //  Usos comunes
--- test.js
+++ test.js
@@ -1,14 +1,2 @@
 'use strick'
 
-var mensaje = 'Llegaban por montones, primero 10, despues 20, despues 35 y se duplicaban hasta llegar a 1000';
-
-//encontrar un patron un numero repetitivo de manera global
-var patron = /\b(\d+)\b/g;
-
-// donde guardaremos le evaluacion
-var match;
-
-while(match = patron.exec(mensaje)){
-    console.log(match);
-    console.log('Se encontro',match[1], ' en ', match.index);
-}
