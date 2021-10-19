410efd31a54b2877e96b8c93822094b7ee4abc10
Agustin-T <65094491+Agustin-T@users.noreply.github.com>
2021-10-19 11:05:48 -0300

    seaching expression with while
    > test()

--- test.js
+++ test.js
@@ -1,1 +1,15 @@
 'use strick'
+
+var mensaje = 'Llegaban por montones, primero 10, despues 20, despues 35 y se duplicaban hasta llegar a 1000';
+
+//encontrar un patron un numero repetitivo de manera global
+var patron = /\b(\d+)\b/g;
+
+// donde guardaremos le evaluacion
+var match;
+
+while(match = patron.exec(mensaje)){
+    console.log(match);
+    console.log('Se encontro',match[0], ' en ', match.index);
+    
+}
