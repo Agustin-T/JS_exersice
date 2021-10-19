027a3e9a8d62db6c71907c40dfa1187d9acbceec
Agustin-T <65094491+Agustin-T@users.noreply.github.com>
2021-10-19 10:39:41 -0300

    add to big file
    > expresionesRegulares.js
    > test.js

--- expresionesRegulares.js
+++ expresionesRegulares.js
@@ -232,9 +232,34 @@ console.log(mensaje3.search(/\S/));
 console.log(mensaje4.search(/\W/));
 
 //-----------------------------------------------------------
-//  La propiedad LasrIndex en expresiones regulares
+//  La propiedad LastIndex en expresiones regulares
 
+console.log('------------------------------------------');
+var expresion1 = /y/g;
 
+console.log(expresion1.lastIndex);
+
+expresion1.lastIndex = 6;
+console.log(expresion1.lastIndex);
+
+var coincidencia = expresion1.exec('sirb yfx sibr yfx');
+
+console.log(coincidencia);
+console.log(expresion1.lastIndex);
+
+var expresion2 = /\d/g;
+console.log('------------------------------------------');
+console.log(expresion2.exec('Pedi 4 tacos y 1 agua'));//5
+console.log(expresion2.exec('Pedi 4 tacos y 1 agua'));//15
+console.log(expresion2.exec('Pedi 4 tacos y 1 agua'));//null
+
+console.log('------------------------------------------');
+var expresion3 = /\d/g;
+console.log(expresion3.exec('Pedi 4 tacos y 1 agua'));//5
+console.log(expresion3.exec('Pedi 4 tacos y 1 agua'));//15
+//vuelve al ultimo indice (4)
+expresion3.lastIndex = 6;
+console.log(expresion3.exec('Pedi 4 tacos y 1 agua'));//15
 
 //-----------------------------------------------------------
 //  ciclos  y evaluaciones en la expresiones regulares
