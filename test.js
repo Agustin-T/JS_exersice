8ad1d753f0e98c4d367fc38c07948dbfec3a19c0
Agustin-T <65094491+Agustin-T@users.noreply.github.com>
2021-10-19 12:34:06 -0300

    validation of mail
    > test.js
    > index.html

--- test.js
+++ test.js
@@ -1,2 +1,32 @@
-'use strick'
+// tools.left.org/html/rfc5322
+// www.regular-expressions.ingo/email.htmls
 
+// not work, see later
+/*
+$(function (){
+    
+})
+*/
+
+function testEmail(){
+    var contacForm = document.getElementById('contactForm');
+
+    contacForm.onsubmit = function(e){
+        e.preventDefault();
+
+        //console.log('El boton ha sido pulsado');
+        console.log('El correo es valido?', isValidEmail());
+    }
+
+    var isValidEmail = function(){
+        var email = document.getElementById('email')
+        // ^ al inicio
+        // W cualquier tipo de letra
+        // + uno o mas repeticiones
+        
+        // \.\+\-] si hay un simbolo
+        // \? puede o no existir
+        // ()* puede estar una o mas veces
+        return /^\w+([\.\+\-]?\w+)*@([\.-]?w+)*(\.\w{2,4})+$/.test(email.value)
+    }
+};
