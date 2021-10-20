7e29f04bf153babff21e297b2c508ec2c9bb4c2d
Agustin-T <65094491+Agustin-T@users.noreply.github.com>
2021-10-20 08:55:41 -0300

    add to big file
    > expresionesRegulares.js

--- expresionesRegulares.js
+++ expresionesRegulares.js
@@ -318,7 +318,21 @@ function testEmail(){
 //-----------------------------------------------------------
 //  Validacion de Twitter user
 
+function testEmail(){
+    var contacForm = document.getElementById('contactForm');
 
+    contacForm.onsubmit = function(e){
+        e.preventDefault();
+
+        console.log('El correo es valido?', isValidEmail());
+    }
+
+    var isValidEmail = function(){
+        var twitter = document.getElementById('email');
+        // in the input: @tomas / tomas
+        return /^@?(\w+)$/.test(twitter.value);
+    }
+};
 
 //-----------------------------------------------------------
 //  Expresiones Regulares
