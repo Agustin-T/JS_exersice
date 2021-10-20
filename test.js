7b19130e7a97330752b21fdc86398af53e7a59b4
Agustin-T <65094491+Agustin-T@users.noreply.github.com>
2021-10-20 08:54:57 -0300

    twitter account validation
    > test.js

--- test.js
+++ test.js
@@ -1,22 +1,15 @@
-$(function testEmail(){
+function testEmail(){
     var contacForm = document.getElementById('contactForm');
 
     contacForm.onsubmit = function(e){
         e.preventDefault();
 
-        //console.log('El boton ha sido pulsado');
         console.log('El correo es valido?', isValidEmail());
     }
 
     var isValidEmail = function(){
-        var email = document.getElementById('email');
-        // ^ al inicio
-        // W cualquier tipo de letra
-        // + uno o mas repeticiones
-        
-        // \.\+\-] si hay un simbolo
-        // \? puede o no existir
-        // ()* puede estar una o mas veces
-        return /^\w+([\.\+\-]?\w+)*@([\.-]?w+)*(\.\w{2,4})+$/.test(email.value);
+        var twitter = document.getElementById('email');
+        // in the input: @tomas / tomas
+        return /^@?(\w+)$/.test(twitter.value);
     }
-});
+};
